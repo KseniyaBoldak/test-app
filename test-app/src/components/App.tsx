231f579a96824df58dc4c82dc135a35f6feca9c4
Kseniya Boldak <kseniya.boldak@gmail.com>
2022-11-05 21:03:17 +0300

    changed content, added Button component

---
+++ test-app/src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Button from "./Button/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h3>This is React Application</h3>
+        <Button>Hello</Button>
+        <Button>How are you?</Button>
+    </div>
+  );
+}
+
+export default App;
