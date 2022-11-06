e14cb30d52a84b496426f9dd2b724a937ae8cb4d
Kseniya Boldak <kseniya.boldak@gmail.com>
2022-11-06 22:34:09 +0300

    added Calculator component

--- test-app/src/components/App.tsx
+++ test-app/src/components/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
-import Counter from './Counter/Counter';
-
+import Calculator from './Calculator/Calculator';
 
 function App() {
+ 
   return (
     <div className="App">
-        <Counter/>
+        <Calculator></Calculator>
     </div>
   );
 }
